495fb656787ef97e000f8717a055b2e61a0ec1cd
Ahf_Programmer <ahf_Programmer@gmail.com>
2026-09-18 21:31:49 +0330

    Add and Setup Retrofit

--- app/src/main/java/com/ahf2025/myapplication/data/api/network/NetworkModule.kt
+++ app/src/main/java/com/ahf2025/myapplication/data/api/network/NetworkModule.kt
@@ -1,4 +1,49 @@
 package com.ahf2025.myapplication.data.api.network
 
+import com.ahf2025.myapplication.data.api.request.FoodApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+import kotlin.jvm.java
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+    private const val BASE_URL="https://api.nal.usda.gov/fdc/v1/"
+    private const val API_KEY="VahKsw3tQHZS3BZpPoLy8awiE8iwwXn4YBVOMyZE"
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(ApiKeyInterceptor(API_KEY))
+            .addInterceptor(logging)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideFoodApiService(retrofit: Retrofit): FoodApiService {
+        return retrofit.create(FoodApiService::class.java)
+    }
 }
+
